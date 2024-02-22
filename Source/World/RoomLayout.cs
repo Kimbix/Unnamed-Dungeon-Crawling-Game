f65dd973685038f6ae91b124cde15a725995a2e1
ruskki <theawsomegirl2004@gmail.com>
2024-02-22 18:57:16 -0400

    mines backdrop

--- Source/World/RoomLayout.cs
+++ Source/World/RoomLayout.cs
@@ -2,7 +2,7 @@ namespace Unnamed_Dungeon_Crawling_Game;
 
 public class RoomLayout {
   public enum Space {
-    Null,
+    Null, //hrlp
     Rock,
   }
   public readonly Space[,] SolidGrid = new Space[15, 9];
